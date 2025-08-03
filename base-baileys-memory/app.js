96b899f9bd49eeb72dd05713cc424519f001d1be
Nerio Villalobos <nervill@gmail.com>
2025-08-03 13:41:15 -0300

    Funcional

--- base-baileys-memory/app.js
+++ base-baileys-memory/app.js
@@ -1,4 +1,8 @@
 // app.js
+// Modificacion funcional- > luego de la correccion de errores de compilacion
+// Modificado por: Nervill
+// Fecha: 2024-01-15
+// Version: 1.0.0.BETA-20240115-v0.1
 const { createBot, createProvider, createFlow, addKeyword, EVENTS } = require('@bot-whatsapp/bot')
 
 const QRPortalWeb = require('@bot-whatsapp/portal')
